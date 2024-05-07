14d9c207fd5bd981bd8f891d9634c08731c0f344
竹内桂 <keitake1207@gmail.com>
2024-05-07 10:23:54 +0900

    document.write削除

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 for(let i = 1; i <= 20; i++){
     if(i % 15 === 0){
-    document.write("FizzBuzz");
     console.log("FizzBuzz");
     
     }else if(i % 3 === 0){
-    document.write("Fizz");
     console.log("Fizz");
     
     }else if(i % 5 === 0){
-    document.write("Buzz");
     console.log("Buzz");
     
     }else{
-    document.write(i);
     console.log(i);
     }
     }
